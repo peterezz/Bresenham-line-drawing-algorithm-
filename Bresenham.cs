46b095300ade24f94583ac3a2ff450390b4ba34f
peterezzat <55260283+peterezz@users.noreply.github.com>
2021-09-18 14:15:43 +0200

    project files

---
+++ Bresenham.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bresenham
+{
+    class Bresenham
+    {
+        public List<int> lineBres(int x0, int y0, int xEnd, int yEnd)
+        {
+            int dx = Math.Abs(xEnd - x0);
+            int dy = Math.Abs(yEnd - y0);
+            int x, y, p =( 2 * dy) - dx;
+            int twody = 2 * dy;
+            int twodymindx = 2 * (dy - dx);
+            List<int> pionts = new List<int>();
+            
+            if (x0 > xEnd)
+            {
+                x = xEnd;
+                y = yEnd;
+                xEnd = x0;
+            }
+            else
+            {
+
+                x = x0; y = y0;
+            }
+            //arr
+            pionts.Add(x);
+            pionts.Add(y);
+            
+            while (x<xEnd)
+
+            {
+               
+                x++;
+                if (p<0)
+                {
+                    p = p + twody;
+                }
+                else
+                {
+                    y++;
+                    p = p + twodymindx;
+                }
+                pionts.Add(x);
+                pionts.Add(y);
+
+            }
+
+
+            return pionts;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bresenham
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x0 = int.Parse(textBox1.Text);
+            int y0 = int.Parse(textBox2.Text);
+            int xend = int.Parse(textBox4.Text);
+            int yend = int.Parse(textBox3.Text);
+            lineBres(x0, y0, xend, yend);
+            void lineBres(int x0, int y0, int xEnd, int yEnd)
+            {
+                int dx = Math.Abs(xEnd - x0);
+                int dy = Math.Abs(yEnd - y0);
+                int x, y, p = (2 * dy) - dx;
+                int twody = 2 * dy;
+                int twodymindx = 2 * (dy - dx);
+                var aBrush = Brushes.Black;
+                var g = panel1.CreateGraphics();
+              
+
+                if (x0 > xEnd)
+                {
+                    x = xEnd;
+                    y = yEnd;
+                    xEnd = x0;
+                }
+                else
+                {
+
+                    x = x0; y = y0;
+                }
+                //arr
+
+                g.FillRectangle(aBrush,x, y, 2, 2);
+                while (x < xEnd)
+
+                {
+
+                    x++;
+                    if (p < 0)
+                    {
+                        p = p + twody;
+                    }
+                    else
+                    {
+                        y++;
+                        p = p + twodymindx;
+                    }
+                    g.FillRectangle(aBrush, x, y, 2, 2);
+
+                }
+
+
+               
+            }
+
+           
+          
+
+
+        }
+    }
+}
